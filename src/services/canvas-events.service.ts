b064e25e1fe80a277a233bf0d079253e90fb33db
Anton Vidinev <anton.vidinev@da-14.com>
2021-06-23 16:10:19 +0300

    fix drop area inactive in few seconds after hover

--- src/services/canvas-events.service.ts
+++ src/services/canvas-events.service.ts
@@ -16,7 +16,7 @@ export class CanvasEventsService {
   private dragLeftDelta: number;
   private canvas: Canvas;
   private activeDropArea: IDropAreaGroup | null = null;
-  private dropAreasAndPolygons: { area: IDropAreaGroup, polygon: Polygon }[] = [];
+  private dropAreasAndPolygons: { area: IDropAreaGroup; polygon: Polygon }[] = [];
 
   static isDragEventAllowed(target?: Object) {
     return target && target.type === ObjectTypes.state && target.selectable;
@@ -78,14 +78,19 @@ export class CanvasEventsService {
         this.canvas.remove(activeObject);
       }
     });
-    this.canvas.on('after:render', debounce(() => {
-      this.dropAreasAndPolygons.forEach(({ area }: any) => {
-        if (area.isActive()) {
-          area.toggleActive(false);
-          this.canvas.renderAll();
-        }
-      });
-    }, 100));
+    this.canvas.on(
+      'after:render',
+      debounce(() => {
+        this.dropAreasAndPolygons.forEach(({ area }: any) => {
+          if (area.isActive()) {
+            if (!this.canvas.getActiveObject()) {
+              area.toggleActive(false);
+              this.canvas.renderAll();
+            }
+          }
+        });
+      }, 100),
+    );
     this.canvas.on('object:moved', (event: IEvent) => {
       this.currentDragTop = 0;
       this.canvas.remove(this.canvas.getActiveObject());
@@ -98,23 +103,28 @@ export class CanvasEventsService {
     this.setupDragOverflowEvents();
   }
 
-  private prepareDropAreaPolygons(dropAreas: IDropAreaGroup[]): { area: IDropAreaGroup, polygon: Polygon }[] {
+  private prepareDropAreaPolygons(dropAreas: IDropAreaGroup[]): { area: IDropAreaGroup; polygon: Polygon }[] {
     return dropAreas.map((dropArea: IDropAreaGroup) => {
       const dropAreaLeft = dropArea.getLeft();
       const dropAreaTop = dropArea.getTop();
-      const dropAreaPoints = [{
-        x: dropAreaLeft,
-        y: dropAreaTop
-      }, {
-        x: dropAreaLeft + dropAreaSize.width,
-        y: dropAreaTop
-      }, {
-        x: dropAreaLeft + dropAreaSize.width,
-        y: dropAreaTop + dropAreaSize.height
-      }, {
-        x: dropAreaLeft,
-        y: dropAreaTop + dropAreaSize.height
-      }];
+      const dropAreaPoints = [
+        {
+          x: dropAreaLeft,
+          y: dropAreaTop,
+        },
+        {
+          x: dropAreaLeft + dropAreaSize.width,
+          y: dropAreaTop,
+        },
+        {
+          x: dropAreaLeft + dropAreaSize.width,
+          y: dropAreaTop + dropAreaSize.height,
+        },
+        {
+          x: dropAreaLeft,
+          y: dropAreaTop + dropAreaSize.height,
+        },
+      ];
       return {
         area: dropArea,
         polygon: new fabric.Polygon(dropAreaPoints, {
@@ -123,8 +133,8 @@ export class CanvasEventsService {
           objectCaching: false,
           transparentCorners: false,
           visible: false,
-        })
-      }
+        }),
+      };
     });
   }
 
--- src/services/workflow-builder.ts
+++ src/services/workflow-builder.ts
@@ -15,7 +15,6 @@ import { WorkflowDimensions } from '../models/interfaces/workflow dimentions.int
 import { tick } from './tick.service';
 
 /*
- * Fix drop area inactive in few seconds after hover
  * Fix sorting cases https://www.loom.com/share/7bb1ad688b5e42a78adb91aaaa61d2fa
  * Disable pass state drag
  * Implement zoom
